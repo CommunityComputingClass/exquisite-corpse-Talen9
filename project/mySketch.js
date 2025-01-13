4cc8c756acab7310f06aa60a32c5b8e5ef17a11d
Talen9 <164447845+Talen9@users.noreply.github.com>
2025-01-13 22:55:52 +0000

    submitting my first code

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,19 +1,31 @@
 function setup() {
   createCanvas(400, 400);
   background(220);
+  
+}
 
-  fill(255); // White color for the eyes
-  stroke(0); // Black outline
-  ellipse(100, 100, 65, 50); // Left eye
-  ellipse(200, 100, 65, 50); // Right eye
-
-  fill(0); // Black color for pupils
-  ellipse(100, 100, 20, 20); // Left pupil
-  ellipse(200, 100, 20, 20); // Right pupil
-
-  fill(255); 
-  ellipse(101, 95, 5, 5);
-  ellipse(201, 95, 5, 5);
+function draw (){
+face(mouseX,mouseY);
 
 }
+function face(x,y){
+  fill ("green")
+  ellipse (x+50,y+30, 300, 300)
+  
+  fill(255); // White color for the eyes
+  stroke(0); // Black outline
+  ellipse(x, y, 65, 50); // Left eye
+  ellipse(x+100, y, 65, 50); // Right eye
+
+  fill(0); // Black color for pupils
+  ellipse(x, y, 20, 20); // Left pupil
+  ellipse(x+100, y, 20, 20); // Right pupil
+
+  fill(255); 
+  ellipse(x+1, y-5, 5, 5);
+  ellipse(x+101, y-5, 5, 5);
+
+
+}
+
 
